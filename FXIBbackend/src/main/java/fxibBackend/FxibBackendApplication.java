a88f95c71dc90de622ebc269450fa38999a2a270
StanSNS <stanimirsergev159@gmail.com>
2023-12-06 18:51:01 +0200

    update spring version to 3.2.0

--- FXIBbackend/src/main/java/fxibBackend/FxibBackendApplication.java
+++ FXIBbackend/src/main/java/fxibBackend/FxibBackendApplication.java
@@ -1,6 +1,5 @@
 package fxibBackend;
 
-import fxibBackend.exception.AccessDeniedException;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@OpenAPIDefinition(info = @Info(title = "FXIB - Backend API",version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "FXIB - Backend API", version = "1.0"))
 public class FxibBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(FxibBackendApplication.class, args);
